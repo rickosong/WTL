c5592033815c27862130ab7a0d8390bc4e8e7401
rickosong <ricko.aprilianto00@gmail.com>
2024-03-15 11:43:30 +0800

    init

---
+++ belajar-php-kuliah/pertemuan1/tugas1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tentang saya</title>
+</head>
+<body>
+    <?php
+        echo "Nama : Mohammad Ricko Aprilianto<br>";
+        echo "Kelas : 4A Bisnis Digital<br>";
+        echo "NIM : E030422015<br>";
+        echo "Sekilas Tentang saya : Saya seorang Mahasiswa Bisnis Digital semester 4 di Politenik Negeri Banjarmasin, saya mempunyai hobi seperti bermain Badminton dan bersepeda, saya merupakan Alumni dari SMK Negeri 2 Banjarmasin Jurusan Rekayasa Perangkat Lunak sehingga saya cukup memahami dan lumayan mendalami terkait pelajaran di bidang pemrograman, sekarang saya menimba ilmu di Prodi Bisnis Digital karena ingin mendalami pemahaman tentang berbisnis di era digital seperti sekarang. sekian sekilas tentang saya terimakasih. <br>";
+    ?>
+</body>
+</html>
